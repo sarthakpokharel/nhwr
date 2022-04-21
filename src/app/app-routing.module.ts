e6730b5e30087a39c63a42d33fa3e031afa7b689
Sarthak Pokharel <sarthakpokharel@gmail.com>
2022-04-21 11:43:06 +0545

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { Home1Component } from './home1/home1.component';
 
 const routes: Routes = [
-
-  {
-    path: 'home',
-    component:HomeComponent
-  },
   {
     path: 'home1',
     component: Home1Component
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 import { Home1Component } from './home1/home1.component';
 import { MapsComponent } from './maps/maps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     Home1Component,
     MapsComponent
   ],
